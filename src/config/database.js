d01a9fd5b7f101d5cd7dbcd9990b85e5ed9e72d8
Pedro <pedro.henriksantosp@gmail.com>
2022-11-17 08:10:52 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+module.exports={
+    dialect:"mysql",
+    host:"localhost",
+    username:"<User aqui>",
+    password:"<Senha do db>",
+    database:"dbstreaming",
+    define:{
+        timestamps:false
+    }
+}
